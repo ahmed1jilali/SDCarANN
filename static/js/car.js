405c73ab31907522018b12f77b49bcb8140124ac
ahmed1jilali <ahmed1jilali@gmail.com>
2025-02-15 18:45:05 +0100

    polygon based car rendering

--- static/js/car.js
+++ static/js/car.js
@@ -7,12 +7,13 @@ class Car {
 
     this.speed = 0;
     this.acceleration = 0.1;
-    this.maxSpeed = 5;
+    this.maxSpeed = 7;
     this.friction = 0.05;
     this.angle = 0;
 
     this.sensor = new Sensor(this);
     this.controls = new Controls()
+    this.polygon = this.#createPolygon();
   }
 
   #move() {
@@ -56,24 +57,42 @@ class Car {
     this.y -= Math.cos(this.angle) * this.speed;
   }
 
+  #createPolygon() {
+    const points = [];
+    const rad = Math.hypot(this.width, this.height) / 2;
+    const alpha = Math.atan2(this.width, this.height);
+    points.push({
+      x: this.x - Math.sin(this.angle - alpha) * rad,
+      y: this.y - Math.cos(this.angle - alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(this.angle + alpha) * rad,
+      y: this.y - Math.cos(this.angle + alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
+    });
+    points.push({
+      x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+      y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
+    });
+    return points;
+  }
+
   update(roadBorders) {
     this.#move();
+    this.polygon = this.#createPolygon();
     this.sensor.update(roadBorders);
   }
 
   draw(ctx) {
-    ctx.save();
-    ctx.translate(this.x, this.y);
-    ctx.rotate(-this.angle);
     ctx.beginPath();
-    ctx.rect(
-      -this.width / 2,
-      -this.height / 2,
-      this.width,
-      this.height
-    )
+    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+    for (let i = 1; i < this.polygon.length; i++) {
+      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+    }
     ctx.fill();
-    ctx.restore();
     this.sensor.draw(ctx);
   }
 
