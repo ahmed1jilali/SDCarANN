20e5dc7bfd108d2da5a55bbab49ad6be28e5fc19
ahmed1jilali <ahmed1jilali@gmail.com>
2025-02-16 16:11:01 +0100

    mutate the best car brain + generate more traffic

--- static/js/main.js
+++ static/js/main.js
@@ -7,16 +7,30 @@ networkCanvas.clientWidth = 500;
 const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const traffic = [
-  new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY")
-];
+// const traffic = [
+//   new Car(road.getLaneCenter(0), -100, 30, 50, "DUMMY"),
+//   new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY"),
+//   new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY"),
+//   new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY"),
+//   new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY"),
+//   new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY"),
+//   new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY"),
+//   new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY"),
+// ];
+const traffic = generateTraffic(20);
 
 // const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 7);
-const cars = generateCars(300);
+const cars = generateCars(500);
 let bestCar = cars[0];
 
-if (localStorage.getItem("bestCar")) {
-  bestCar.brain = JSON.parse(localStorage.getItem("bestCar"));
+if (localStorage.getItem("bestBrain")) {
+  console.log("hello")
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+    if (i != 0) {
+      NeuralNetwork.mutate(cars[i].brain, 0.1)
+    }
+  }
 }
 
 
@@ -86,4 +100,18 @@ function save() {
 function discard() {
   localStorage.removeItem("bestBrain");
 
+}
+
+function generateTraffic(N) {
+  const traffic = [];
+  for (let i = 0; i < N; i++) {
+    traffic.push(new Car(
+      road.getLaneCenter(Math.floor(Math.random() * 3)),
+      -Math.floor(Math.random() * 3000),
+      30,
+      50,
+      "DUMMY"
+    ));
+  }
+  return traffic;
 }
